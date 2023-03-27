f84dc2a3f9a71a2e1a5689e15a7b3c1d01ef3521
Harry Alexander <b9034879@newcastle.ac.uk>
2023-03-27 19:21:32 +0100

    Created UI functionality and adjusted game manager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,49 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Transform cityPrefab;
-    public Transform planePrefab;
-    public Transform plane;
+    // City generator prefabs
+    public GameObject lindenmayerPrefab;
+    public GameObject voronoiPrefab;
 
-    public GameObject roadGeneratorPrefab;
-    private GameObject roadGenerator;
+    // Terrain prefabs
+    public GameObject planePrefab;
 
-    public GameObject cityBlockGeneratorPrefab;
-    private GameObject cityBlockGenerator;
+    // Canvas
+    public GameObject canvas;
+
+    private GameObject cityGenerator;
+    private GameObject terrain;
+  
 
     // Start is called before the first frame update
     void Start()
     {
-        CreateRoadGenerator(new(0, 0, 0));
-        
-        //CreatePlane(new(45, 0, 213), new(25, 1, 15));
-        //CreateCity();
+       
     }
 
     private void Update()
     {
-        //CreateCityBlockGenerator(new(0, 0, 10));
+        
     }
 
-  /*  void CreateCity() {
-        Vector3 cityPos = new(plane.position.x - (plane.localScale.x * 5), plane.position.y, plane.position.z - (plane.localScale.z * 5));
-        Transform city = Instantiate(cityPrefab, cityPos, Quaternion.identity);
-        city.SetParent(plane);
+    public void CreateCityGenerator(Vector3 pos, GameObject cityGeneratorPrefab) {
+       cityGenerator = Instantiate(cityGeneratorPrefab, pos, Quaternion.identity);
     }
 
-    void CreatePlane(Vector3 pos, Vector3 dim) {
-        plane = Instantiate(planePrefab, pos, Quaternion.identity);
-        plane.localScale = dim;
-    }*/
-
-    void CreateRoadGenerator(Vector3 pos) {
-       roadGenerator = Instantiate(roadGeneratorPrefab, pos, Quaternion.identity);
+    public void CreateTerrrain(Vector3 pos, GameObject terrainPrefab) {
+        terrain = Instantiate(terrainPrefab, pos, Quaternion.identity);
+        terrain.tag = "Terrain";
     }
-
-   /* void CreateCityBlockGenerator(Vector3 pos) {
-        if (Input.GetMouseButtonDown(0)) {
-            Debug.Log("Pressed primary button.");
-            cityBlockGenerator = Instantiate(cityBlockGeneratorPrefab, pos, Quaternion.identity);
-        }*/
        
   }
